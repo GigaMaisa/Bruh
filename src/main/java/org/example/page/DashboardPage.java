23f356b5bc40977472c1ee0ed49c215650b7af2c
Giga Maisuradze <giga.maisuradze.1@btu.edu.ge>
2023-11-27 23:11:34 +0400

    ideis gaziareba to

--- src/main/java/org/example/page/DashboardPage.java
+++ src/main/java/org/example/page/DashboardPage.java
@@ -8,13 +8,28 @@ import static com.codeborne.selenide.Selenide.$;
 @Getter
 public class DashboardPage {
     /*
-    * krch esaa page klasi, logikurad vyoft imis mixedvit tu sad vimyofebit:
-    * DashboardPage, HomePage, LoginPage, ProfilePage, SettingsPage da ase sh.
-    * aq ra elementebic gvchirdeba imat velebs vqmnit logikuri saxelebit da daaxloebit
-    * ase gamoiyureba yvela page klasi
+    * axla idea ra aris: titoeul elements davadot rame action anotacia, sadac gadavcemt tu ra gvinda:
     */
 
+    //Actions{CLICK, VISIBLE, ENTER}
     private final SelenideElement startApplicationButton = $("a[href$='.aspx']");
+
+    //Actions{NOT_VISIBLE, SELECT, HOVER}
     private final SelenideElement goHomeButton = $("button[value='home']");
+
+    //Actions{EMPTY, EXIST, FILL, HAS_TEXT}
     private final SelenideElement nameInput = $("input[value='name']");
+
+    /*
+     * da ro dagenerirdes metodebi igive paternit: clickFieldName, validateFieldNameIsRagaca, hoverFieldName
+     * validateFieldNameHasText(String text) {
+     *      fieldName.shouldHave(Condition.text(text));
+     * }
+     */
+
+    /*
+    * es tavdapirveli ideaa, mara ra da rogor moxdeba poxui, mtavaria stepebi vagenerirot, tu gamova
+    * lombokis msgavsad, tu arada shegvidzlia prosta sxva carieli step klasi mivutitot sadme da iq davagenerirot .java failshi
+    * rac iqneba shesrulebadi is iyos ra ;d
+    * */
 }
